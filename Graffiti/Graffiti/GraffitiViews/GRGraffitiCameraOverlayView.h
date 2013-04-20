cc21669b164d4b99824707318b57525f63b4aafa
Demetri Miller <demetri.miller@gmail.com>
2013-04-20 15:48:20 -0500

    Pulled Graffiti app out of Demo folder in PARWorks-iOS repo into its own. Much cleaner now.

---
+++ Graffiti/Graffiti/GraffitiViews/GRGraffitiCameraOverlayView.h
@@ -0,0 +1,17 @@
+//
+//  GRGraffitiCameraOverlayView.h
+//  Graffiti
+//
+//  Created by Demetri Miller on 3/6/13.
+//  Copyright (c) 2013 Demetri Miller. All rights reserved.
+//
+
+#import "GRCameraOverlayView.h"
+
+@interface GRGraffitiCameraOverlayView : GRCameraOverlayView
+
+
+/// Methods overridden from superclass.
+- (void)cancelButtonTapped:(id)sender;
+
+@end
